ccc4df526db77ffcc37cc241c70a79e82a7ee02b
sandibekibraimov <sandibek@gmail.com>
2021-11-12 13:22:50 -0500

    reverse str exercise

---
+++ reversestring/index.js
@@ -0,0 +1,32 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+// solution #3
+function reverse(str) {
+  return str.split('').reduce((reversed, character) => {
+    return character + reversed;
+  }, '');
+}
+
+module.exports = reverse;
+
+//solution #1
+// function reverse(str) {
+//   return str.split('').reverse().join('');
+// }
+
+// solution #2
+// function reverse(str) {
+//   let reversed = '';
+
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+
+//   return reversed;
+// }
