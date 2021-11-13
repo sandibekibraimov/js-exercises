5e41b89f113ff933b15eee84599714b456390788
sandibekibraimov <sandibek@gmail.com>
2021-11-12 22:38:53 -0500

    maxchar added

---
+++ maxchar/index.js
@@ -0,0 +1,33 @@
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = '';
+
+  // iterating through string to make an object
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+
+  // iterating through object by comparing max and maxChar
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
+
+module.exports = maxChar;
